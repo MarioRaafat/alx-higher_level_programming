a4b3464d8e603bc40bf1d3c3af9759d9f06e095e
MarioRaafat <136023677+MarioRaafat@users.noreply.github.com>
2023-10-09 13:01:56 +0300

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,90):
+for i in range(0, 90):
     if i == 89:
         print("{}".format(i))
     elif i % 10 > i / 10:
-        print("{:02}, ".format(i), end = "")
+        print("{:02}, ".format(i), end="")
     else:
         pass
