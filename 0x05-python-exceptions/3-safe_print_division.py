bbc6aff7704c99fa4bcc9c88727c73144e715d10
{your <mariorafat10@gmail.com>
2023-10-25 16:24:59 -0700

    task3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except (TypeError, ZeroDivisionError):
+        result = None
+    print("Inside result: {}".format(result))
