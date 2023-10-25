c9720302851c4bc6d0e2db9e5c568533a9a2e40f
{your <mariorafat10@gmail.com>
2023-10-25 16:10:20 -0700

    task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             n += 1
-        except IndexError:
+        except (ValueError, TypeError):
             continue
     print("")
     return n
