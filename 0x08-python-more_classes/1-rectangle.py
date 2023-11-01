c74ce014a90202b17eb64a1731ff8221055cd047
MarioRaafat <136023677+MarioRaafat@users.noreply.github.com>
2023-11-01 23:17:31 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Define the class"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represent it"""
+    """Represent a rectangle."""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
         Args:
