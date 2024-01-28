eed37bf520ed1704f50cbfed0128489afdccaf6c
MarioRaafat <mariorafat10@gmail.com>
2024-01-28 09:38:13 -0800

    task10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,6 @@
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
     for i in range (0, len(my_list)):
-        new_list[i] = my_list[i] % 2
+        new_list[i] = not( my_list[i] % 2)
         i += 1
     return new_list
