cfd966aeb40c707dc9b17e108434db95c3c63a62
MarioRaafat <mariorafat10@gmail.com>
2024-01-29 18:44:31 -0800

    task13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,36 +8,77 @@
   *
   * Return: 0 if it is not a palindrome, 1 if it is a palindrome
   */
-int is_palindrome(listint_t** head)
+int is_palindrome(listint_t **head)
 {
-    listint_t* top = *head;
-    listint_t* rear = *head;
-    listint_t* temp = *head;
-    int len = 1;
-
-    while (rear->next) {
-        rear = rear->next;
-        len++;
-    }
+    listint_t *start = NULL, *end = NULL;
+    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
 
     if (head == NULL)
         return (0);
 
     if (*head == NULL)
         return (1);
+    
+    start = *head;
+    len = listint_len(start);
+    len_cyc = len * 2;
+    len_list = len_cyc - 2;
+    end = *head;
 
-    for(int i = 0; i < len/2; i++)
+    for (; i < len_cyc; i = i + 2)
     {
-        if (rear->n == top->n)
-        {
-            len -= 2;
-            rear = temp;
-            top = top->next;
-            for (int j = 0; j < (len + i); j++)
-                rear = rear->next;
-        }
-        else
-            return 0;
+        if (start[i].n != end[len_list].n)
+            return (0);
+
+        len_list = len_list - 2;
     }
-    return 1;
+
+    return (1);
 }
+
+/**
+  * get_nodeint_at_index - Gets a node from a linked list
+  * @head: The head of the linked list
+  * @index: The index to find in the linked list
+  *
+  * Return: The specific node of the linked list
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int iter_times = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (iter_times == index)
+				return (current);
+
+			current = current->next;
+			++iter_times;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+  * slistint_len - Counts the number of elements in a linked list
+  * @h: The linked list to count
+  *
+  * Return: Number of elements in the linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+
+	return (lenght);
+}
+
