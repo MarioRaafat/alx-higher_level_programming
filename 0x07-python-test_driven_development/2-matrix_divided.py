7a8ac2b104b143856254c17dd9c40752461ae4a3
MarioRaafat <mariorafat10@gmail.com>
2024-02-10 21:06:13 -0800

    task1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-"""defines function to scalar divde matrix"""
+"""Defines a matrix division function."""
 
 
 def matrix_divided(matrix, div):
-    """divides matrix by scalar integer, rounded to two decimal places"""
+    """Divide all elements of a matrix.
+
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
+    """
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     sum = 0
     value = len(matrix[1])
     for i in matrix:
