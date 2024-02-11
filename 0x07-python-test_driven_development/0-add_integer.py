8ee37b9c05433d324e60d1a3d615945e68737e65
MarioRaafat <mariorafat10@gmail.com>
2024-02-10 20:09:13 -0800

    tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this file contain a function that can add 2 numbers and return the integer sum
+it converts the float numbers to int
+only using int or float 
+"""
+
+def add(a, b = 98):
+    """ Addition of two integers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
