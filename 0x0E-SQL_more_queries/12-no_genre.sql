036a40d16f70a21e869b5e7f91c166168bdd5b5b
MarioRaafat <mariorafat10@gmail.com>
2024-03-15 19:06:11 -0700

    task12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,5 @@
 SELECT ts.title, tsg.genre_id
 FROM tv_shows ts
 JOIN tv_show_genres tsg ON ts.id = tsg.show_id
-WHERE tsg.genre_id = NULL
+WHERE tsg.genre_id IS NULL
 ORDER BY ts.title, tsg.genre_id;
