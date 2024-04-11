f0660e65667c6a45159ca8a0744acf0686b29d55
MarioRaafat <mariorafat10@gmail.com>
2024-04-11 11:45:47 -0700

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,13 @@
+
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const num = Number(process.argv[2]);
+  const x = Number(process.argv[2]);
   let i = 0;
-  while (i < num)){
-    console.log('X'.repeat(num));
+  while (i < x) {
+    console.log('X'.repeat(x));
     i++;
   }
 }
+
