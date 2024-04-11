2d0ada682517e5509bd49d6adc6a072c23853fdb
MarioRaafat <mariorafat10@gmail.com>
2024-04-11 11:14:45 -0700

    task6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-let str_arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let str_arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < str_arr.length; i++) {
   console.log(str_arr[i]);
 }
