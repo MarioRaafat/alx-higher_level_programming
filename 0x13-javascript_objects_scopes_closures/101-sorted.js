7c7803da3c228adf43d7cd945e204d469df19dcf
MarioRaafat <mariorafat10@gmail.com>
2024-04-12 10:28:05 -0700

    2adv

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 const { dict } = require('./101-data');
-
 const usersByOccurrence = {};
-
 for (const userId in dict) {
   const occurrences = dict[userId];
-
   if (!usersByOccurrence[occurrences]) {
     usersByOccurrence[occurrences] = [];
   }
-
   usersByOccurrence[occurrences].push(userId);
 }
-
 console.log(usersByOccurrence);
-
