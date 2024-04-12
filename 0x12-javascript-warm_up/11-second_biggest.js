0c80e09b9f3cf5541080314c2d5b452bc3acd6b3
MarioRaafat <mariorafat10@gmail.com>
2024-04-12 08:11:25 -0700

    task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[3]) || process.argv[3] === undefined) {
   console.log(0);
 } else {
-  let arr = process.argv.slice(2);
+  let arr = process.argv.slice(2).map(Number);
   let second = arr.sort( function (a, b) { b - a; })[1];
   console.log(second);
 }
