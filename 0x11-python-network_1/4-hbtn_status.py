0a8cdca2a59484b1bc437d71619dcdeba17138df
MarioRaafat <mariorafat10@gmail.com>
2024-06-14 17:37:03 -0700

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
 
-    content = requsts.get("https://alx-intranet.hbtn.io/status")
+    content = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(content.text)))
     print("\t- content: {}".format(content.text))
