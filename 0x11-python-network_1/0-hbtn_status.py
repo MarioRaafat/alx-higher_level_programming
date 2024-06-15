a4f1fd25123e9082db01e086cc72824b9c29c000
MarioRaafat <mariorafat10@gmail.com>
2024-06-14 17:13:39 -0700

    ya_rb

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     res = urllib.request.urlopen('https://alx-intranet.hbtn.io/status').read()
 
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode('utf-8')))
